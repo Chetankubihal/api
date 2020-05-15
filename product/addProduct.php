0ba2ba6c24be7c054ca2b755ad82fbe7b688f370
kubihalchetan <kubihalchetan@gmail.com>
2020-05-15 11:35:08 +0000

    added api

---
+++ product/addProduct.php
@@ -0,0 +1,73 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once '../config/database.php';
+ 
+// instantiate ag$products object
+include_once '../resources/product.php';
+ 
+$database = new Database();
+$db = $database->getConnection();
+ 
+$product = new Product($db);
+
+// get posted data
+
+// make sure data is not empty
+
+ 
+    $product->product_SKU = '"'. $_POST["product_sku"].'"';
+    $product->product_HSN ='"'.$_POST["product_hsn"].'"';
+    $product->product_name ='"'.  $_POST["product_name"].'"';
+    $product->product_description ='"'. $_POST["product_description"].'"';
+    $product->product_category = '"'. $_POST["product_category"].'"';
+    $product->product_sub_category='"'.  $_POST["product_sub_category"].'"';
+    $product->seller_email='"'.  $_POST["seller_email"].'"';
+    $product->product_MRP='"'.  $_POST["product_MRP"].'"';
+    $product->product_selling_price='"'.  $_POST["product_selling_price"].'"';
+    $product->package_length='"'.  $_POST["package_length"].'"';
+    $product->package_width='"'.  $_POST["package_width"].'"';
+    $product->package_breadth='"'.  $_POST["package_breadth"].'"';
+    $product->package_weight='"'.  $_POST["package_weight"].'"';
+
+    // $product->product_MRP='"'.  $_POST["product_MRP"].'"';
+    // $product->product_selling_price='"'.  $_POST["product_selling_price"].'"';
+
+    
+
+    if($product->addProduct())
+    {
+
+        
+
+        $current_directory=getcwd();
+        //concatenate current_directory with product_id
+        $current_directory=$current_directory."/product_images/".trim($product->seller_email,'"')."/";
+
+        //making directory by product_id
+        if(!is_dir($current_directory))
+        mkdir($current_directory,0777);
+
+        $current_directory=$current_directory.trim($_POST['product_sku'],'"')."/";
+
+        if(!is_dir($current_directory))
+        mkdir($current_directory,0777);
+
+        http_response_code(200);
+        echo json_encode(array("message"=>"True","product_id"=>$product->product_id,"product_SKU"=>$_POST['product_sku']));
+    }
+
+    else
+    {
+
+        http_response_code(404);
+
+        echo json_encode(array("message"=>"False"));
+    }
+
+?>
---
+++ resources/product.php
@@ -0,0 +1,237 @@
+<?php
+
+class Product{
+
+private $conn;
+private $table = 'products';
+
+public $product_id;
+public $product_SKU;   
+public $product_HSN; 
+public $product_name;
+public $product_category;
+public $product_sub_category;
+public $product_description;
+public $product_MRP;
+public $product_selling_price;
+public $merchant_id;
+public $seller_email;
+public $status;
+public $package_length;
+public $package_width;
+public $package_breadth;
+public $package_weight;
+
+
+public function __construct($con)
+{
+    $this->conn = $con;
+}
+
+function category_name()
+{
+    $query="select category_name from category where category_id = ".trim($this->product_category,'"');
+
+    
+    $stmt=$this->conn->prepare($query);
+
+    $stmt->execute();
+
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+	
+ 
+    $this->product_category= '"'.$row['category_name'].'"';
+
+
+}
+
+function sub_category_name()
+{
+	$query="select sub_category_name from sub_category where category_id = ".trim($this->product_category,'"') ." AND sub_category_id = ".trim($this->product_sub_category,'"');
+
+
+    $stmt=$this->conn->prepare($query);
+
+    $stmt->execute();
+
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $this->product_sub_category='"'. $row['sub_category_name'] .'"';
+
+
+}
+function addProduct()
+{
+	$this->sub_category_name();
+
+	$this->category_name();
+
+    $query="INSERT INTO products(product_SKU,product_HSN,product_name,product_description,product_category,product_sub_category,seller_email,product_MRP,product_selling_price,package_length,package_width,package_breadth,package_weight) values (".$this->product_SKU.','.$this->product_HSN.','.$this->product_name.','.$this->product_description.','.$this->product_category.','.$this->product_sub_category.','.$this->seller_email.','.$this->product_MRP.','.$this->product_selling_price.','.$this->package_length.','.$this->package_width.','.$this->package_breadth.','.$this->package_weight.")";
+
+  
+    $stmt= $this->conn->prepare($query);
+
+   if($stmt->execute())
+   {
+       //query to get last product id and increment by one
+       $query1="SELECT MAX(product_id) as id from products where seller_email=".$this->seller_email;
+
+       $stmt1=$this->conn->prepare($query1);
+
+       if($stmt1->execute())
+            {
+                $row = $stmt1->fetch(PDO::FETCH_ASSOC);
+                $this->product_id=$row['id'];
+               # $this->product_id=$row['product_SKU'];
+
+                return true;
+            }
+        else 
+            {
+             return false;
+            } 
+   }
+
+   else
+   {
+       return false;
+   }
+
+}
+
+function getCategory()
+{
+    $query="SELECT * From category";
+
+    $stmt= $this->conn->prepare($query);
+
+   $stmt->execute();
+
+   return $stmt;
+
+}
+function getSubCategory($category_id)
+{
+    $query="SELECT * From sub_category WHERE category_id=".$category_id;
+
+    $stmt= $this->conn->prepare($query);
+
+   $stmt->execute();
+
+   return $stmt;
+
+}
+
+function readOne(){
+ 
+    // query to read single record
+     $query = "SELECT *
+            FROM
+                " . $this->table . "
+            WHERE
+                product_id = " . $this->product_id . "
+            LIMIT
+                0,1";
+ 
+    // prepare query statement
+    $stmt = $this->conn->prepare( $query );
+ 
+    // execute query
+    if ($stmt->execute())
+ {
+    // get retrieved row
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+ 
+    // set values to object properties
+    // $this->product_name = $row['product_name'];
+    // $this->product_description = $row['product_description'];
+    // $this->product_category = $row['product_category'];
+    // $this->product_sub_category = $row['product_sub_category'];
+    $this->product_MRP = $row['product_MRP'];
+    $this->product_selling_price = $row['product_selling_price'];
+
+    // $this->product_SKU = $row['product_SKU'];
+    // $this->product_HSN = $row['product_HSN'];
+    // $this->status = $row['status'];
+
+    return true;
+ }
+ else
+ {
+     return false;
+ }
+    
+  
+}
+
+function updateProduct()
+{
+    $query="Update products SET product_MRP=".$this->product_MRP.",product_selling_price=".$this->product_selling_price.",package_length=".$this->package_length.",package_width=".$this->package_width.",package_breadth=".$this->package_breadth.",package_weight=".$this->package_weight."WHERE  product_id=".$this->product_id."AND seller_email=".$this->seller_email;
+
+  
+    $stmt= $this->conn->prepare($query);
+
+   if($stmt->execute())
+   {
+        return true;   
+   }
+
+   else
+   {
+       return false;
+   }
+
+}
+
+function excelUpload($sheet_data,$seller_email)
+{
+   $flag=0;
+
+    foreach ($sheet_data as $row)
+    {
+        if(!empty($row['C']) && $row['C']!='Name' )
+        {
+
+                $query='INSERT INTO products (product_SKU,product_HSN,product_name,product_description,product_category,product_sub_category,seller_email,product_MRP,product_selling_price,package_length,package_width,package_breadth,package_weight) values ("'.$row['A'].'","'.$row['B'].'","'.$row['C'].'","'.$row['D'].'","'.$row['E'].'","'.$row['F'].'","'.$seller_email.'","'.$row['G'].'","'.$row['H'].'","'.$row['I'].'","'.$row['J'].'","'.$row['K'].'","'.$row['L'].'") ';
+                $stmt= $this->conn->prepare($query);
+
+                if(!$stmt->execute())
+                {
+                     $flag++;   
+                }
+
+  else
+                {
+                    $current_directory=getcwd();
+                    //concatenate current_directory with product_id
+                    $current_directory=$current_directory."/product_images/".trim($seller_email,'"')."/";
+            
+                    //making directory by product_id
+                    if(!is_dir($current_directory))
+                    mkdir($current_directory);
+            
+                    $current_directory=$current_directory.$row['A']."/";
+            
+                    if(!is_dir($current_directory))
+                    mkdir($current_directory);
+                }       }
+
+    }
+
+    return $flag;
+
+}
+function checkSKUCode()
+{
+    $query = "SELECT product_name from products where product_SKU=".$this->product_SKU." AND seller_email = " .$this->seller_email;
+
+    $stmt=$this->conn->prepare($query);
+
+    $stmt->execute();
+
+    return $stmt;
+
+}
+}
+
+?>
