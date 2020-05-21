b88e5b47c322dac20d8dd94e95f7b716b06768b3
kubihal21 <kubihal21@gmail.com>
2020-05-21 11:33:07 +0530

    Create login.php

---
+++ shopclues/login.php
@@ -0,0 +1,34 @@
+<?php
+
+$oauth_url = 'https://auth.shopclues.com/loginToken.php';   
+$credentials = array(
+    "username" => "kubihal21@gmail.com",
+    "password" => md5("Stek@2020"),
+    "client_id"=> "7ED1GGL7LT8DQ08ZD1SR19Z03LAAZZ5W49OAGZ",
+    "client_secret"=> "YL(3651T9Q&JD^*#6L#6)Y02QZ_R76QJ*L#2&R+46^$989ZS_Q",
+    "grant_type"=> "password"
+    );
+
+function genToken($oauth_url, $credentials)
+{
+ 
+$curl = curl_init($oauth_url);
+curl_setopt_array($curl, array(
+CURLOPT_RETURNTRANSFER => 1,
+CURLOPT_HTTPHEADER => array(
+'Content-Type: application/json'
+),
+CURLOPT_POST => 1,
+CURLOPT_POSTFIELDS => json_encode($credentials)
+));
+$response = curl_exec($curl);
+curl_close($curl);
+$response = json_decode($response, true);
+return $response;
+}
+
+$token = genToken($oauth_url, $credentials);  
+print_r($token) ;  
+
+?>
+
